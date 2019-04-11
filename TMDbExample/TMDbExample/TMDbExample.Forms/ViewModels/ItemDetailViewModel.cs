43d5dadd0784b66566548025f36a7c50e6401fde
Anderson Bragiatto <atbamparo@hotmail.com>
2019-04-11 03:05:50 -0300

    adding VS Xamarin new project template

---
+++ TMDbExample/TMDbExample/TMDbExample.Forms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TMDbExample.Models;
+
+namespace TMDbExample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
