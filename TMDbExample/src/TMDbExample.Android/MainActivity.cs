9d37d35d9a744ffa4badcfc00b1ea1b6866d4cd9
Anderson Bragiatto <atbamparo@hotmail.com>
2019-04-14 03:37:45 -0300

    added icon and splash screen

--- TMDbExample/src/TMDbExample.Android/MainActivity.cs
+++ TMDbExample/src/TMDbExample.Android/MainActivity.cs
@@ -5,17 +5,15 @@ using TMDbExample.Forms;
 
 namespace TMDbExample.Droid
 {
-    [Activity(Label = "TMDbExample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TMDbExample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(false);
             LoadApplication(new App());
         }
     }
---
+++ TMDbExample/src/TMDbExample.Android/SplashActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TMDbExample.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        void SimulateStartup()
+        {
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(false);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
